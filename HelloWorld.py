c7a3ee49ab8f1f14b9df6747a31089a7fc274987
Tack2525459 <Siriwat.boonbanlu@gmail.com>
2025-06-13 14:36:03 +0700

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,30 @@
+print("Hello")
+#คำสั่งในการแสดงผลข้อความ
+
+number = 10 # ตัวแปรประเภทจำนวนเต็ม Integer
+
+str(number) # เปลี่ยนจาก int เป็น String
+# การ Casing แปลงชนิดของข้อมูล
+
+
+total = 97.44 # ตัวแปรประเภททศนิยม Float
+float (total)
+
+name = "siriwat" # ตัวแปรประเภทข้อความ string
+status = True
+# ตัวแปรประเภทตรระศาสตร์
+# True = จริง
+# False = เท็จ 
+
+print(number,total,name,status)
+
+sore = 2+2/3
+print(sore)
+
+
+
+nickname = 1
+str(nickname)
+
+print (nickname + nickname)
+print (str(nickname)+ str(nickname))
